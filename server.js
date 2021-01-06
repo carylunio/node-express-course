4bbabc6bf1ef977d7f7f9077cc6ed0be343f363a
carylunio <medehojunior@gmail.com>
2021-01-06 12:35:29 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-/*
+//Middleware
+app.use(bodyParser.json());
+
 //Get usage
 const mockUserData = [
     { name: 'Mark' },
@@ -17,7 +20,7 @@ app.get('/users', function (requete, response) {
         message: 'successfully got users. Nice!',
         users: mockUserData
     })
-})*/
+})
 
 // Use of variable in URL
 app.get('/users/:name',(requete,response)=>{
@@ -27,6 +30,28 @@ app.get('/users/:name',(requete,response)=>{
 		message: 'got one user',
 		user: requete.params.name
 	})
+})
+
+//Making a post Route
+app.post('/login',function(requete,response){
+    const username=requete.body.username;
+    const password=requete.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+        response.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+        response.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
 })
 
 app.listen(8000, function () {
