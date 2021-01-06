d20cd5649e483599d2bc1bed6afe916007ee020b
carylunio <medehojunior@gmail.com>
2021-01-06 12:14:46 -0500

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    { name: 'Mark' },
+    { name: 'Jill' },
+    {
+        name: 'Caryle',
+        surname: 'MEDEHO',
+    }
+]
+app.get('/users', function (requete, response) {
+    response.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
+
+
 app.listen(8000, function () {
     console.log("le serveur est en cours d'exécution")
 })
